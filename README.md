34eb11fa2994673f82b9a06635b30fb6237c3236
Daniel Dickerson <daniel@SimpleLTC.local>
2019-01-23 10:55:14 -0600

    Updated readme and added some documentation to the Global.asax

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # WslTest
+
+This repository is a test project that documents the inconsistent behavior between this repository when checkouted via gitbash vs windows subsystem for linux (WSL). Recently, per-directory case sensitivity was introduced to improve the interoperability with WSL [source](https://blogs.msdn.microsoft.com/commandline/2018/02/28/per-directory-case-sensitivity-and-wsl/). This seems to be the change that initiated at least one of the issues documented here.
+### gitbash behavior
+
+Works as expected
+
+### WSL behavior
+
+1. Code in Global.asax.cs is not executed resulting in a 404.
+1. ConfigurationManager does not load settings from Web.config
+
+### Workarounds
+
+1. Renaming Global.asax and Global.asax.cs to global.asax and global.asax.cs and updating the csproj to reflect these changes will cause the code in the global.asax.cs to execute.
+1. No workaround for loading the Web.config settings has been found yet.
--- WslTest/Global.asax.cs
+++ WslTest/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,15 @@ namespace WslTest
     {
         protected void Application_Start()
         {
+            // Two different inconsistent behaviors can be tested by either cloning via gitbash or WSL
+            // A clone via gitbash appears to work as expected, however a clone via WSL no longer works.
+            // * The framework will execute code in the Global.asax.cs file unless the Global.asax and Global.asax.cs
+            //   files are renamed to global.asax and global.asax.cs respectively.
+            // * The ConfigurationManager WILL NOT load settings from the Web.config
+            var count = ConfigurationManager.ConnectionStrings.Count;
+            var name = ConfigurationManager.ConnectionStrings[0]?.Name ?? "Undefined";
+            var connectionString = ConfigurationManager.ConnectionStrings[0]?.ConnectionString ?? "Undefined";
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
